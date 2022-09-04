807c429ff88700185161a7d631addc99687a0b5a
xTCry [Vladislav Kh] <wolt6@mail.ru>
2022-09-04 05:29:29 +0300

    feat: added link to schedule viewer in header

--- src/containers/App.container.tsx
+++ src/containers/App.container.tsx
@@ -4,14 +4,18 @@ import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FormControl from '@mui/material/FormControl';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import EventNoteIcon from '@mui/icons-material/EventNote';
 
 import { ThemeModeButton } from '../providers/ThemeMode.provider';
 import MainPageContainer from './MainPage.container';
 import VersionComponent from '../components/Version.component';
 import * as envUtils from '../utils/env.utils';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
 
 const Copyright = () => {
     return (
@@ -43,7 +47,7 @@ const App = () => {
             <AppBar
                 position="absolute"
                 color="default"
-                elevation={0}
+                elevation={5}
                 sx={{
                     position: 'relative',
                     borderBottom: (t) => `1px solid ${t.palette.divider}`,
@@ -53,9 +57,24 @@ const App = () => {
                     <Typography variant="h6" color="inherit" noWrap>
                         [YSTUty] <b>I</b>mport <b>C</b>alendar <b>S</b>chedule
                     </Typography>
-                    <FormControl sx={{ pl: 1 }}>
+                    <Typography sx={{ flex: 1 }}></Typography>
+                    <Divider orientation="vertical" flexItem />
+                    <FormControl>
                         <ThemeModeButton />
                     </FormControl>
+                    {envUtils.linkToScheduleViewer && (
+                        <>
+                            <Divider orientation="vertical" flexItem />
+                            <FormControl>
+                                <Link href={envUtils.linkToScheduleViewer} color="inherit">
+                                    <IconButton>
+                                        <EventNoteIcon />
+                                    </IconButton>
+                                    Schedule
+                                </Link>
+                            </FormControl>
+                        </>
+                    )}
                 </Toolbar>
             </AppBar>
             <Container component="main" maxWidth="md" sx={{ mb: 4 }}>
